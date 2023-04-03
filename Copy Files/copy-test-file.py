4a07c505d9234ee82bd6f2bfcb6eb3c0ac763857
Marvel Okafor <CZ507F@NAM.CORP.GM.COM>
2023-04-03 18:34:38 -0500

    created the copoy-test-file for blockpoint upgrades - NOT COMPLETED

---
+++ Copy Files/copy-test-file.py
@@ -0,0 +1,132 @@
+import sys, re, os
+from netmiko import ConnectHandler, file_transfer
+from netmiko.ssh_exception import NetMikoTimeoutException
+from netmiko.ssh_exception import SSHException
+from netmiko.ssh_exception import AuthenticationException
+from datetime import date, datetime  
+
+
+
+#print("This is sys.argv 0 ->",sys.argv[1])
+#date.today())+
+#str(datetime.now().strftime("%m-%d-%y-[%H-%M]"))
+start_time = datetime.now()
+
+''' -------------------------------------
+    --------- CREDENTIAL OPTIONS --------
+    ------------------------------------- '''
+as_id = input("Insert your as_id: ")
+as_pass = input("\nInsert your as_password: ")
+en_secret = input("\nInsert your secret password:")
+#This is useful for switches without management template. It will use the enable secret
+if en_secret == '':
+    en_secret = as_pass
+
+#print(en_secret)
+''' -----------------------------------
+    ------- PRECECK or POST CHECK -----
+    ----------------------------------- '''
+pre_or_post = "report"
+    
+
+''' ---------------------------------------
+    ---------- DIRECTORY MOVE -------------
+    --------------------------------------- '''
+dir = str(pre_or_post)+ "\\+Logs-" + str(datetime.now().strftime("%b-%d-%y [%H-%M]"))
+#This is for the directory
+if os.path.exists(dir):
+    os.chdir(dir)
+    print("Folder exists")
+else:
+    os.makedirs(dir)  #new dir for files to be moved
+    os.chdir(dir)
+    print("\n"+str(dir) +" has been created!\n")
+
+''' ---------------------------------------
+    ---------- PASTE IP ADDRESSES ---------
+    --------------------------------------- '''
+print("\n\nPaste the IP Addresses you want prechecks generated. \n"+
+"When you are done. Hit ENTER, Ctrl + z, and ENTER in that order to end:\n")
+
+ipAd = sys.stdin.readlines() #This will read multiple lines
+
+
+
+''' ------------------------------------------
+    ---------- ACTUAL IMPLEMENTATION ---------
+    ------------------------------------------ '''
+for i in range(len(ipAd)):
+    if len(ipAd[i]) != 1: #this takes care of empty lines so they are not looked at
+        #print(str(i) +" -> "+ str(userInput[i]) + " length -> " + str(len(userInput[i])))
+        net_dev = {"host":ipAd[i],
+                    "username":as_id,
+                    "password":as_pass, 
+                    "device_type":"cisco_ios",
+                    "secret":en_secret
+                    }
+        try:
+            con = ConnectHandler(**net_dev)
+        except:
+            issue_st = "\nThere is a problem with your log in credentials for " +str(ipAd[i]).strip()+". Please check your username or password.\n"
+            print(issue_st)
+            logs_err = open("ZZ-error-logs.txt", mode="a")
+            logs_err.write(issue_st)
+            continue #This should break out of the loop
+        #con.enable()
+        
+        config_commands = [
+        f"copy ftp://Ftponly:bp1s4fun@10.59.254.7/ios/current/cat9k_iosxe.17.06.04.SPA.bin flash:",
+        "\n"]
+        
+        
+        output = con.send_config_set(config_commands)
+        print(output)
+        
+        
+        
+        '''source_file = r"tftp://10.59.254.7/ios/current/cat9k_iosxe.17.06.04.SPA.bin"
+        dest_file = "test-cat9k_iosxe.17.06.04.SPA.bin"
+        direction = "put"
+        file_system = "flash:"
+        transfer_dict = file_transfer( 
+            con,
+            source_file = source_file,
+            dest_file = dest_file,
+            file_system = file_system,
+            direction = direction,
+            overwrite_file = True,
+        )
+        
+        print(transfer_dict)'''
+        
+        
+        '''scriptFile = open("N:\\Scripts\\COMPLETED\\preCheckLogGenerator\\scripts\\"+str(script_to_use), "r")#we're going back a directory because we changed to the precheck directory
+        fileName = con.send_command("sh run | i (hostname )", read_timeout=180)
+        preCheck_file = open(str(fileName.split(" ")[1])+ pr_po_name, mode="a") #create precheck_file file in .txt for pfcn switches
+        
+        listObj = scriptFile.readlines()
+        
+        
+        for j in listObj:
+            to_send = j
+            log = con.send_command(to_send, read_timeout = 180)#, read_timeout=0, last_read = 8.0)
+            preCheck_file.write("\n"+str(fileName.split(" ")[1]) + "#"+ str(to_send)) #preLog file and preCheck_file...
+            preCheck_file.write(log)
+            #print(to_send)
+            #print(log)
+
+        preCheck_file.close()
+        
+        scriptFile.close()'''
+            
+    if i != len(ipAd) - 1: #do not print this on the last switch
+        print("\n--------------------------------------------------------------------------")    
+        print("----------------------- Moving on to " +str(ipAd[i + 1]).strip() + " -------------------------")
+        print("--------------------------------------------------------------------------\n\n") 
+        
+print("Successfully Completed. Please the " + str(dir) +" Folder")
+
+elapsed_time = datetime.now() - start_time #to calculate the total elapsed time the script took to run
+print("This script took approximately {}".format(elapsed_time))
+        
+        
